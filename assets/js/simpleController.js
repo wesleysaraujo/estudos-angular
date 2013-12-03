1704db60da6dfb12a638d2b87afed9faa3fa2bfd
Wesley S. Araújo <wsadesigner@gmail.com>
2013-12-03 00:29:54 -0200

    Melhorando a exibição do aviso quando o número for 0

--- assets/js/simpleController.js
+++ assets/js/simpleController.js
@@ -6,13 +6,18 @@ function simpleController($scope)
 //Controller countController
 function countController($scope)
 {
-	//Proprieade counter
+	//Variável counter
 	$scope.counter = 0;
 
+	//Variável aviso
+	$scope.aviso = "";
 	//Métoda que adiciona + 1
 	$scope.addOne = function()
 	{
 		$scope.counter++;
+		if($scope.aviso != ""){
+			$scope.aviso = "";
+		}
 	}
 	//Método que subtrai 1
 	$scope.subOne = function()
@@ -21,8 +26,8 @@ function countController($scope)
 		if($scope.counter > 0){
 			$scope.counter--;
 		}else{
-			alert('Está zerado');
-		}	
+			$scope.aviso = "Já tá zerado";
+		}
 	}
 
 	//Array contendo as frutas que serão repetidas
